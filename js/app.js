3ff0fa4c3ae8401f7c3cc45cf77619666b74f9c8
verooroaltamirano <srtaoro@gmail.com>
2021-09-30 11:30:10 -0300

    js1

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/*trabajo js1
+console.log("hola mundo")
+document.write("<h3 class='color2'> hola </h3>")
+let maxi="chau"
+let maxi2="buenos dias"
+console.log(maxi)
+maxi= "hasta nmañana"
+console.log(maxi)
+console.log(maxi2)
+const PI= 3.14
+console.log(PI)
+let numero1= 45
+console.log(numero1)
+const MES="julio"
+console.log(MES)
+*/
+let num1=34
+let num2=23
+let num3=12
+let num4=2
+let sum=num1+num2
+let rest=num2-num3
+let divi= num3/num4
+let multi= num1* num4
+console.log(sum)
+
+
